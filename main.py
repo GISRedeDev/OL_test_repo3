6bad3c5eac0461420454cb9cd2165dfa0202b675
GIS_Rede <109098317+GISRedeDev@users.noreply.github.com>
2025-08-20 08:52:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 def main():
-    output_bronze = Path(__file__).resolve().parent.parent.joinpath("mnt/output/bronze/vm3.txt")
-    output_silver = Path(__file__).resolve().parent.parent.joinpath("mnt/output/silver/vm3.txt")
-    output_gold = Path(__file__).resolve().parent.parent.joinpath("mnt/output/gold/vm3.txt")
+    output_bronze = Path(__file__).resolve().parent.parent.parent.joinpath("mnt/output/bronze/vm3.txt")
+    output_silver = Path(__file__).resolve().parent.parent.parent.joinpath("mnt/output/silver/vm3.txt")
+    output_gold = Path(__file__).resolve().parent.parent.parent.joinpath("mnt/output/gold/vm3.txt")
     output_files = [output_bronze, output_silver, output_gold]
     for output_file in output_files:
         if not output_file.parent.exists():
